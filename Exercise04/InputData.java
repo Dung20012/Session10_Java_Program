82f283727cafad5b9a534aa7d4dd62d490c07740
Dung20012 <hantho1107@gmail.com>
2025-06-10 10:13:08 +0700

    Add files via upload

---
+++ Exercise04/InputData.java
@@ -0,0 +1,57 @@
+package homeword.Exercise04;
+
+import java.util.Scanner;
+
+public class InputData {
+    public static String getString(Scanner scanner, String string){
+        String input = "";
+        do {
+            System.out.println(string);
+            input = scanner.nextLine();
+            if (input.isEmpty()){
+                System.out.println("Vui lòng k để trống !");
+            }
+        }while (input.isEmpty());
+        return input;
+    }
+
+    public static int getInt(Scanner scanner, String string){
+        String rs = "";
+        do {
+            System.out.println(string);
+            rs = scanner.nextLine();
+            try {
+                if (rs.isEmpty()){
+                    System.out.println("Vui lòng k bỏ trống");
+                } else if (Integer.parseInt(rs) < 0) {
+                    System.out.println("Vui lòng nhập so nguyên >= 0 !");
+                }else {
+                    break;
+                }
+            }catch (Exception e){
+                System.out.println("Vui lòng k bỏ trốn");
+            }
+        }while (true);
+        return Integer.parseInt(rs);
+    }
+
+    public static double getDouble(Scanner scanner, String string){
+        String rs = "";
+        do {
+            System.out.println(string);
+            rs = scanner.nextLine();
+            try {
+                if (rs.isEmpty()){
+                    System.out.println("Vui lòng k bỏ trống");
+                } else if (Double.parseDouble(rs) < 0) {
+                    System.out.println("Vui lòng nhập số thực >= 0");
+                }else {
+                    break;
+                }
+            }catch (Exception e){
+                System.out.println("Vui lòng nhập số thực >= 0");
+            }
+        }while (true);
+        return Double.parseDouble(rs);
+    }
+}
